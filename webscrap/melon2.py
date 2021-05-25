8bf8296c40418ebc34dd1716a3999ca7cc33bce8
yunnyisgood <cyeon0801@gmail.com>
2021-05-25 12:20:39 +0900

    commit

--- webscrap/melon2.py
+++ webscrap/melon2.py
@@ -5,33 +5,34 @@ class Melon(object):
     url = ''
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 
+    @staticmethod
+    def ranking(soup, value, type):
+        count = 0
+        for i in soup.find_all("div", {"class": value}):
+            count += 1
+            print(f'{str(count)} 위')
+            print(f' {type}: {i.find("a").text}')
 
     @staticmethod
     def main():
         melon = Melon()
         while 1:
-            menu = int(input('0. Exit\n  1. Input URL 2.Title Ranking\n 3.Artist Ranking'))
+            menu = int(input('0. Exit\n  1. Input URL\n 2.Ranking'))
             if menu == 0:
                 break
             elif menu == 1:
                 melon.url = requests.get(input('url을 입력하세요'), headers=melon.headers).text
             elif menu == 2:
                 soup = BeautifulSoup(melon.url, 'lxml')
-                count = 0
-                for i in soup.find_all("div", {"class":"ellipsis rank01"}):
-                    count += 1
-                    print(f'{str(count)} 위')
-                    print(f' Title: {i.find("a").text}')
-            elif menu == 3:
-                soup = BeautifulSoup(melon.url, 'lxml')
-                count = 0
-                for i in soup.find_all("div", {"class":"ellipsis rank02"}):
-                    count += 1
-                    print(f'{str(count)} 위')
-                    print(f'Artist: {i.find("a").text}')
+                print("<<Artist Ranking>>")
+                melon.ranking(soup, "ellipsis rank01", "title")
+                print("<<Title Ranking>>")
+                melon.ranking(soup, "ellipsis rank02", "artist")
+
             else:
                 print('다시 입력하세요')
                 continue
                 # https://www.melon.com/chart/index.htm
+                # test
 
 Melon.main()
